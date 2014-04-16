26d71d87f2d13607024e95a80275d094ecc12f17
Tom MacWright <tom@macwright.org>
2014-04-16 17:12:16 -0400

    fix blank symbols

--- index.js
+++ index.js
@@ -115,7 +115,11 @@ function loadMaki(options, callback) {
 function loadCached(options, callback) {
     var base = options.base + '-' + options.size,
         size = options.size,
+        symbol;
+
+    if (options.symbol) {
         symbol = options.symbol + '-' + options.size + (options.retina ? '@2x' : '');
+    }
 
     if (!base || !size || !markerCache.base[base] ||
         (symbol && !markerCache.symbol[symbol])) {
